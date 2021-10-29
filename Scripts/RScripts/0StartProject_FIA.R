328f1b8a9c06bccf494c960cdc88701db341c7e9
pmaurogut <pmaurogut@gmail.com>
2021-10-29 09:16:28 -0700

    Intersects created with new location id
    Add more fields and Suggestions from Dimitrios

--- Scripts/RScripts/0StartProject_FIA.R
+++ Scripts/RScripts/0StartProject_FIA.R
@@ -44,28 +44,6 @@ add_plot_fields<-function(x){
 	
 }
 
-#function to make FIRST CN CROSSWALK TABLE
-add_intersects_fields<-function(x){
-#	add plot fields and sets default values
-	x$index_used<-NA
-	x$downloaded_raw_tiles<-FALSE
-	x$downloaded_fom<-NA
-	x$downloaded_las_version<-NA
-	x$merged_lidar<-FALSE
-	x$projected_lidar<-FALSE
-	x$has_time_stamp<-FALSE
-	x$min_time<-NA
-	x$max_time<-NA
-	x$has_classes<-FALSE
-	x$new_classification<-FALSE
-	x$DTM_created<-FALSE
-	x$metrics_created
-	x$plot_path<-paste("STATES",x$STATECD,x$LOCATION_ID,sep="/")
-	x
-	
-}
-
-#Aux functions to prepare the folders 
 make_folders_region<-function(x,y="PLOTS_LIDAR"){
 	try({
 				dir.create(x)
@@ -74,18 +52,6 @@ make_folders_region<-function(x,y="PLOTS_LIDAR"){
 				dir.create(paste(x,y,sep="/"))
 			})
 }
-
-make_folders_project_plot<-function(x){
-	try({
-				dir.create(x)
-				dir.create(paste(x,"PC",sep="/"))
-				dir.create(paste(x,"DTM",sep="/"))
-				dir.create(paste(x,"METRICS",sep="/"))
-			})
-}
-
-#function to make FIRST CN CROSSWALK TABLE
-
 
 #GET PLOT DATA FROM FIADB
 con <- dbConnect(RSQLite::SQLite(), FIADB)
--- Scripts/RScripts/1Create_intersects_projects_FIA.R
+++ Scripts/RScripts/1Create_intersects_projects_FIA.R
@@ -2,25 +2,53 @@
 # 
 # Author: gutierrf
 ###############################################################################
-library("USGSlidar")
-library("rFIA")
 library("plyr")
 library("purrr")
 library("sp")
 library("rgdal")
 library("sf")
-library("rgeos")
-library("jsonlite")
 library("doParallel")
-source("Scripts/RScripts/Auxiliary_functions/Downloaders.R")
+
 
 #Destination folder
 DESTCRS<-"epsg:3857"
-INDEXFOLDER<-"FIA_lidar_chips/Data/Index_USGS"
+INDEXFOLDER<-"F:/Paco/CMSWS/FIA_lidar_chips/Data/Index_USGS"
 FIADBFOLDER<-"Data/FIADB"
 DESTFOLDER<-"F:/LIDAR_FIA"
 FOLDER<-paste(DESTFOLDER,"PLOTSFOLDER",sep="/")
 TYPES<-c("ENTWINEPLUS")
+
+add_intersects_fields<-function(x){
+#	add plot fields and sets default values
+	x$index_used<-NA
+	x$downloaded_raw_tiles<-FALSE
+	x$downloaded_fom<-NA
+	x$downloaded_las_version<-NA
+	x$merged_lidar<-FALSE
+	x$projected_lidar<-FALSE
+	x$has_time_stamp<-FALSE
+	x$min_time<-NA
+	x$max_time<-NA
+	x$has_classes<-FALSE
+	x$new_classification<-FALSE
+	x$DTM_created<-FALSE
+	x$metrics_created
+	x$plot_path<-paste("STATES",x$STATECD,x$LOCATION_ID,sep="/")
+	x
+	
+}
+
+make_folders_project_plot<-function(x){
+	try({
+				dir.create(x)
+				dir.create(paste(x,"PC",sep="/"))
+				dir.create(paste(x,"DTM",sep="/"))
+				dir.create(paste(x,"METRICS",sep="/"))
+			})
+}
+
+
+
 #make path to plots shapefiles
 plots_bbox<-paste(FOLDER,"BBOX","BBOX_ALL.gpkg",sep="/")
 states<-unique(st_read(plots_bbox)$STATECD)
@@ -37,32 +65,65 @@ combinations$state<-c("ALL",states)
 #Make destination folder name
 combinations$dest_folder<-dirname(gsub("BBOX/","INTERSECTED_INDEXES/",combinations$plots))
 
-combinations$dest_file<-paste(combinations$dest_folder,"/",
-		basename(combinations$index),sep="")
-combinations$dest_file<-gsub(".gpkg$","",combinations$dest_file)
-combinations$dest_file<-paste(combinations$dest_file,"_",combinations$state,".gpkg",sep="")
 
-combinations<-ddply(combinations,c("index"),function(x){
+combinations$dest_file<-paste(combinations$dest_folder,"/",
+		combinations$state,"_",
+		basename(combinations$index),sep="")
+
+#Sets wd in DESTFOLDER
+#Save only intersects for states
+setwd(DESTFOLDER)
+cores<-max(1,detectCores())
+cl <- makePSOCKcluster(cores,outfile="../../debug.txt")
+loaded<-.packages()
+doParallel::registerDoParallel(cl)
+Sys.time()
+combinations<-ddply(combinations[!combinations$state=="ALL",],c("index","state"),function(x){
 			
 		plots<-st_read(x$plots[1])
 		projects<-st_read(x$index[1])
 		projects<-st_transform(projects,st_crs(plots))
-		ddply(x,"plots",function(y){
-			dir.create(y$dest_folder)
-			success<-try({
-						plots<-st_read(y$plots[1])
-						intersects_projects<-st_intersection(plots,projects)
-						intersects_projects$PC_area_proj<-st_area(intersects_projects)
-						ref_area<-2000^2
-						intersects_projects$PC_prop_proj<-as.numeric(intersects_projects$PC_area_proj)/ref_area
-						st_write(intersects_projects,y$dest_file[1], delete_layer = TRUE)		
-					})
-			y$index_created<-!inherits(success,"try-error")
-			y
-					
-		},.progress="tk")
-			
-		},.progress="tk")
+		success<-try({
+			intersects_projects<-st_intersection(plots,projects)
+			intersects_projects$PC_area_proj<-st_area(intersects_projects)
+			ref_area<-2000^2
+			intersects_projects$PC_prop_proj<-as.numeric(intersects_projects$PC_area_proj)/ref_area
+			st_write(intersects_projects,x$dest_file[1], delete_layer = TRUE)		
+				})
+		x$success<-!inherits(success,"try-error")
+		x	
+		},.parallel=TRUE,.paropts=list(.packages=loaded))
+stopCluster(cl)
+Sys.time()
 
-save(combinations,file="ALL_Intersects_plots.Rdata")
+
+cores<-max(1,detectCores())
+cl <- makePSOCKcluster(cores,outfile="../../debug.txt")
+loaded<-.packages()
+doParallel::registerDoParallel(cl)
+Sys.time()
+combinations_directories<-ddply(combinations[!combinations$state=="ALL",],c("index","state"),function(x){
+			
+			intersects<-st_read(x$dest_file[1])
+			intersects$lidar_plot_folder<-paste(intersects$plot_path,
+					basename(dirname(intersects$url)),sep="/")
+			
+			res<-ddply(intersects,"LOCATION_ID",function(y){
+				a<-try(make_folders_project_plot(y$lidar_plot_folder))
+				y$lidar_plot_folder<-ifelse(inherits(a,"try-error"),NA,
+						y$lidar_plot_folder)
+				y		
+					},.progress="tk")
+			res<-st_sf(res)
+			
+			success<-try({
+						st_write(intersects_projects,x$dest_file[1], delete_layer = TRUE)		
+					})
+			res$success<-!inherits(success,"try-error")
+			res	
+		},.parallel=TRUE,.paropts=list(.packages=loaded))
+stopCluster(cl)
+Sys.time()
+
+save(combinations,combinations_directories,file="STATES/Intersects_states.Rdata")
 

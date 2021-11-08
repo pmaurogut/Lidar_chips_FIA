33c621af3a04cec6113e993663037a33f5a6cd99
pmaurogut <pmaurogut@gmail.com>
2021-11-08 08:06:36 -0800

    Started adding comments to aux

--- Scripts/RScripts/Auxiliary/Pipeline_functions.R
+++ Scripts/RScripts/Auxiliary/Pipeline_functions.R
@@ -2,23 +2,20 @@
 # 
 # Author: pmaurogut
 ###############################################################################
-library("USGSlidar")
 library("plyr")
-library("purrr")
-library("sp")
-library("rgdal")
 library("sf")
 library("RJSONIO")
 
-#Destination folder
-DESTCRS<-"EPSG:5070+5703"
-INDEXFOLDER<-"F:/Paco/CMSWS/FIA_lidar_chips/Data"
-DESTFOLDER<-"D:/CMS_PLOTS"
-FOLDER<-paste(DESTFOLDER,"PLOTSFOLDER",sep="/")
-TESTFOLDER<-"D:/CMS_PLOTS/BACKUP"
-
-
 #Function to replace template with options NOT USED
+#' Title
+#'
+#' @param template 
+#' @param options 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 fill_template<-function(template,options){
 	
 	if(!length(template)==length(options)){
@@ -40,13 +37,38 @@ fill_template<-function(template,options){
 	
 }
 
+#Function to pass a df or sf it writes a file with the pdal pipeline commands to run
+#' 
+#' @param x 
+#' @param pipelines 
+#' @param output 
+#' @returnType 
+#' @return 
+#' 
+#' @author pmaurogut
+#' @export
+create_pipelines_bat<-function(x,pipelines,output="test.bat"){
+	writeLines(paste("pdal pipeline ","\"",x[[pipelines]],"\"",sep=""),con=output)
+}
+
 #function to run a function that generates a json pipeline for each row of
 #a data frame or ssf object. 
 #For each processing step the intersections with the indexes are passed to the function
 #the. The fourth argument of the funtion is another function that generates the pipeline
 #based on the data stored in a row of a df or sf object. These functions should return
 #a list with two strings
-add_json<-function(x,name,write=TRUE, pipelinefunc,...){
+#' 
+#' @param x dataframe or sf object 
+#' @param field_name (string) field name to store the path to the command or json pipeline
+#' @param write should the pipeline be written to disk?
+#' @param pipelinefunc function that creates the pipelines from rows of x
+#' @param ... extra arguments to pipelinefunc
+#' @returnType dataframe or sf object 
+#' @return dataframe or sf object with field_name populated
+#' 
+#' @author pmaurogut
+#' @export
+add_json<-function(x,field_name,write=TRUE, pipelinefunc,...){
 	extra_args<-list(...)
 	adply(x,1,function(y,pipelinefunc,extra_args){
 				args<-list(row=y)
@@ -59,9 +81,9 @@ add_json<-function(x,name,write=TRUE, pipelinefunc,...){
 							}
 						})
 				if(inherits(a,"try-error")){
-					y[,name]<-NA
+					y[,field_name]<-NA
 				}else{
-					y[,name]<-pipeline[2]
+					y[,field_name]<-pipeline[2]
 				}
 				y
 				
@@ -69,8 +91,20 @@ add_json<-function(x,name,write=TRUE, pipelinefunc,...){
 	
 }
 
+#' 
+#' @param row sf with a single row. If row has more than one row only the fist
+#' is used
+#' @param field path to the plot geometry
+#' @param dest_crs 
+#' @returnType 
+#' @return 
+#' 
+#' @author pmaurogut
+#' @export
 download_reproject_pipeline<-function(row,field="bbox_path",dest_crs="EPSG:5070+5703"){
-	geom<-st_read(row[[field]][1])
+	row<-row[1,]
+	geom<-row
+	geom<-st_sf(geom)
 	geom<-st_transform(geom,st_crs(row$EPSG))
 	polygon<-st_as_text(st_geometry(geom))
 	bbox<-st_bbox(geom)
